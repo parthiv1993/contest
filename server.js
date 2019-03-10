99e59b94dc839f457186bdbe6bf8229323e97e9f
parthiv patel <parthiv1993.pp@gmail.com>
2019-03-10 22:54:52 +0530

    typo

--- server.js
+++ server.js
@@ -24,5 +24,5 @@ app.get('/', function (req, res) {
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
-app.listen(port, i);
+app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
