58cf112f929e5cef31c481b0197ef55f2cb4e2c3
parthiv patel <parthiv1993.pp@gmail.com>
2019-05-18 21:26:52 +0530

    removing src

--- src/Constants.js
+++
@@ -1,7 +0,0 @@
-export default {
-    // BASE_URL : 'http://localhost:8080/api',
-    BASE_URL : 'http://wc2019-world-cup.1d35.starter-us-east-1.openshiftapps.com/api',
-    LIVE_POLL_TIME : 5000,
-    POINTS_POLL_TIME : 60000,
-    MYTEAM_POLL_TIME : 60000
-}
