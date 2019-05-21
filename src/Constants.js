6c96bde05194af2db2a23ea5e909509a767fcd4c
Parthiv Patel <parthiv1993.pp@gmail.com>
2019-05-21 12:46:03 +0530

    Update Constants.js

--- src/Constants.js
+++ src/Constants.js
@@ -1,6 +1,6 @@
 export default {
     // BASE_URL : 'http://localhost:8080/api',
-    BASE_URL : 'http://wc2019-world-cup.1d35.starter-us-east-1.openshiftapps.com/api',
+    BASE_URL : 'http://icc-wc2019.1d35.starter-us-east-1.openshiftapps.com/',
     LIVE_POLL_TIME : 5000,
     POINTS_POLL_TIME : 60000,
     MYTEAM_POLL_TIME : 60000
