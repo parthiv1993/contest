361d12f2d4131dd6b0cdd3b1a38293654ce94f89
parthiv patel <parthiv1993.pp@gmail.com>
2021-04-05 16:40:21 +0530

    added players

--- constants/points.js
+++ constants/points.js
@@ -1,11 +1,12 @@
 module.exports={
+   
+    Yuvraj : 1000,
+    Dravid : 1000,
+    Irfan: 1000,
+    ZzAlien : 100000, 
     Kumble : 1000,
     Sehwag : 1000,
     Laxman : 1000,
     Zaheer : 1000,
     Ganguly : 1000,
-    Yuvraj : 1000,
-    Dravid : 1000,
-    Irfan: 1000,
-    ZzAlien : 100000
 }
